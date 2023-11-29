f360d5fb6f0acc31751d3b4ba0698ad170a008c0
marcelabino <134214698+marcelabino@users.noreply.github.com>
2023-11-29 14:41:19 -0300

    atividade 3 - preparação para html e css

---
+++ lojaCosmeticos.py
@@ -0,0 +1,4 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+for i in range(len(lista_produtos)):
+    print("Temos " + lista_produtos[i] + " à venda!")
