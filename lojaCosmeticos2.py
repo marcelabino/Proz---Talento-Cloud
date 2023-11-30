128e1404d84fc1dd020d8f2faaf3b8406b9bff04
marcelabino <134214698+marcelabino@users.noreply.github.com>
2023-11-30 11:00:38 -0300

    atividade 4 - preparação para html e css

---
+++ lojaCosmeticos2.py
@@ -0,0 +1,10 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+lista_produtos.pop() #remover delineadores na última posição
+
+for i in range(len(lista_produtos)):
+    if(lista_produtos[i] == 'batons'): #substituição
+        lista_produtos[i] = 'rímel'
+    if(lista_produtos[i] == 'loções'):
+        lista_produtos[i] = 'cremes hidratantes'
+    print("Temos " + lista_produtos[i] + " à venda!")
