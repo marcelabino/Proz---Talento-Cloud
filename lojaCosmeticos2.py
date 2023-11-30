ce02286839aaaaea303c985f3b9f14af7a8ba903
marcelabino <134214698+marcelabino@users.noreply.github.com>
2023-11-30 11:07:53 -0300

    adição de novos produtos

--- lojaCosmeticos2.py
+++ lojaCosmeticos2.py
@@ -2,9 +2,15 @@ lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loçõ
 
 lista_produtos.pop() #remover delineadores na última posição
 
+
+
 for i in range(len(lista_produtos)):
     if(lista_produtos[i] == 'batons'): #substituição
         lista_produtos[i] = 'rímel'
     if(lista_produtos[i] == 'loções'):
         lista_produtos[i] = 'cremes hidratantes'
-    print("Temos " + lista_produtos[i] + " à venda!")
+    #print("Temos " + lista_produtos[i] + " à venda!")
+
+lista_produtos.append("água micelar")
+lista_produtos.append("corretivo")
+print(lista_produtos)
